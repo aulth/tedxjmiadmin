b7b53c439d342b65984970f0600dab2a1613ff99
aulth <mohdusman.you@gmail.com>
2023-08-31 14:44:56 +0530

    meta tag added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import Home from "@/components/Home";
-
+import Head from "next/head";
 export default function App() {
   return (
     <>
+   <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Admin Portal" />
+        <meta name="author" content="Mohd Usman" />
+        <meta name="keywords" content="TedX, Jmi, Tedx Jmi 2023" />
+        <title>TEDxJMI - Admin</title>
+      </Head>
     <Home/>
     </>
   )
