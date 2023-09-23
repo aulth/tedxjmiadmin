92979a44c03cca20c99b503d34ddab7003620fbd
aulth <mohdusman.you@gmail.com>
2023-09-23 11:24:13 +0530

    entered added

--- src/pages/api/count.js
+++ src/pages/api/count.js
@@ -10,15 +10,19 @@ export default async function handler(req, res) {
     const idSet = new Set();
     const unique = [];
     const duplicate = [];
+    const entered = [];
     for (const obj of tickets) {
+        if (obj.used) {
+            entered.push(obj.email.toLowerCase());
+        }
         if (!idSet.has(obj.email.toLowerCase())) {
             // Duplicate id found, return false
             idSet.add(obj.email.toLowerCase());
             unique.push(obj.email.toLowerCase());
-        }else{
+        } else {
             duplicate.push(obj.email.toLowerCase());
         }
     }
     // return res.json({ success: true, msg: "Ticket Found", tickets: tickets })
-    return res.json({success:false, count:unique.length, total:tickets.length, duplicate:duplicate.length})
+    return res.json({ success: false, count: unique.length, total: tickets.length, duplicate: duplicate.length, entered:entered.length })
 }
